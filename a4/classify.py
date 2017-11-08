f67436aafcf43a8f09d190c5fe1c0dd7b0519b4d
vasireddydivya <vasireddydivi@gmail.com>
2017-11-07 19:51:36 -0600

    Updated comments in classify.py file

--- a4/classify.py
+++ a4/classify.py
@@ -1,5 +1,20 @@
 """
-classify.py
+FILE DESCRIPTION:
+-----------------
+This file contains all methods i use to classify my data as having a positive sentiment towards "Donald Trump" or
+negative sentiment towards "Trump", Here i have annotated tweets obtained from Collect.py  as 1 or 0
+where 1 represents the positive class and 0 represents the negative one. I have annotated labels using Affin dataset.
+then using this as my training set i have
+trained my support vector machine(SVM) classifier using a linear kernel , then i collect my tweets at run time and
+i classify them using my trained SVM.
+Module Requirements for this File:
+
+1) sklearn
+2) nltk
+3) os
+4) pandas
+Here you might need to download the nltk stopwords corpus using the command
+-- nltk download , then select the one mentioned as stopwords corpus and download it.
 """
 
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -12,6 +27,12 @@ import numpy as np
 
 
 def train_test_split(file_name):
+    """
+    This function reads the CSV file name in Collect_Folder. I'm splitting the data into train and test in 65%, 35%
+    respectively. The file is .csv file in the form of (tweet_id,tweet_text,tweet_label).
+    :param file_name: The name of the file to read
+    :return:
+    """
     fields = ['text', 'label']
     twitter_data = pd.read_csv("Collect_Folder" + os.path.sep + file_name, usecols=fields)
     test = set(range(len(twitter_data))[::350])
@@ -20,6 +41,13 @@ def train_test_split(file_name):
     return twitter_data.iloc[train], twitter_data.iloc[test]
 
 def read_data(data_list, flag):
+    """
+        This method reads the test/train data file and creates a list of test/train data and
+        its labels.
+        :param      filename: The name of the file to read
+        :param      flag    : Stating true if train file or false for test file
+        :return:    2 lists, one data list and one label list
+        """
     text = []
     label = []
     for row in data_list.iterrows():
@@ -35,6 +63,12 @@ def read_data(data_list, flag):
 
 
 def vectorize_train_data(data_list, mindf=4, maxdf=0.8):
+    """
+        This method reads the list of tweets then converts it to a csr_matrix using sklearns built-in function
+        we also remove the stopwords from every tweet using nltk's list of stopwords
+        :param data_list: list of tweets
+        :return: csr_matrix containing tf-idf values of the tweets
+        """
     stopword_list = stopwords.words('english')
     vectorizer = TfidfVectorizer(min_df=mindf, max_df=maxdf, sublinear_tf=True, use_idf=True, stop_words=stopword_list)
     data_vector = vectorizer.fit_transform(data_list)
@@ -42,10 +76,25 @@ def vectorize_train_data(data_list, mindf=4, maxdf=0.8):
 
 
 def vectorize_test_data(test_data_list, vectorizer):
+    """
+        :param test_data_list:
+        :param vectorizer:
+        :return:
+        """
     data_vector = vectorizer.fit_transform(test_data_list)
     return data_vector
 
 def classify_data(test_vector,train_vector,train_labels,strategy='linear'):
+    """
+        This method takes in the train and test vectors containing tf-idf scores , train_labels and performs classification
+        using SVM(Support Vector Machine, and uses a linear kernel for the strategy) and returns a list of predicted labels
+        for the test data.
+        :param      test_vector : csr matrix containing tf-idf scores for the test_data
+        :param      train_vector: csr matrix containing tf-idf scores for the train_data
+        :param      train_labels: list of corresponding labels for the train_data_vector
+        :param      strategy    : The type of Kernel to use for a Support Vector Machine
+        :return:  A list of predicted labels for the test data
+        """
     classifier_linear = svm.SVC(kernel=strategy)
     classifier_linear.fit(train_vector,train_labels)
     predictions = classifier_linear.predict(test_vector)
