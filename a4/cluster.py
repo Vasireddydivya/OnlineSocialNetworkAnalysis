3f8092b38149b515d6e10605eeb2d1363731c16b
Vasireddydivya <vasireddydivi@gmail.com>
2017-11-15 15:11:58 -0600

    created graph

--- a4/cluster.py
+++ a4/cluster.py
@@ -27,3 +27,52 @@ import matplotlib.pyplot as plt
 import itertools
 import os
 
+
+def create_graph(filename):
+    G = nx.Graph()
+    with open(filename, 'r') as fp:
+        followers_dict = json.load(fp)
+    new_dict = dict({key: value for key, value in followers_dict.items() if key != 'elonmusk'})
+    G.add_nodes_from(new_dict.keys())
+    for key in new_dict:
+        for follower in new_dict[key]:
+            if follower not in G:
+                G.add_node(follower)
+                G.add_edge(key, follower)
+            elif follower in G and G.has_edge(key, follower) == False and G.has_edge(follower, key) == False:
+                G.add_edge(key, follower)
+    print("\nCreated graph from data in file --> " + filename + "\n")
+    print("\nGraph Contains : ")
+    print("----------------")
+    print("\n " + str(len(G.nodes())) + " Nodes")
+    print("\n " + str(len(G.edges())) + " Edges")
+    return G
+
+
+def save_graph(G):
+    """
+    This creates an image of our original graph that has not undergone clustering .
+    :param      G: Our networkx graph object
+    :return:    Nothing
+    """
+    pos = nx.spring_layout(G, scale=2)
+    plt.axis('off')
+    nx.draw_networkx_nodes(G, pos, alpha=0.5, node_color='r', node_size=20)
+    nx.draw_networkx_edges(G, pos, width=0.4, alpha=0.5)
+    plt.savefig("Cluster_Folder"+os.path.sep+"Graph.png", dpi=300)  # save as png
+
+
+def main():
+    """
+    This method runs the methods defined in this file and saves the details after identifying the clusters and generates
+    a graph before clustering and after clustering.
+    :return: Nothing
+    """
+
+    print("\t\t************************ - Starting cluster.py - ************************ ")
+
+    G = create_graph(filename="Collect_Folder" + os.path.sep + "elonmusk.json")
+    save_graph(G)
+
+if __name__ == '__main__':
+    main()
